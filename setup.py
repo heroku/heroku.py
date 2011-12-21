804238e3c8edd706f1dad01c376749212e3ef5de
Kenneth Reitz <me@kennethreitz.com>
2011-12-21 17:28:22 -0500

    distribute it up

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import heroku
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+required = ['requests>=0.8.6',]
+
+
+setup(
+    name='heroku',
+    version=heroku.__version__,
+    description='Heroku API Wrapper.',
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('HISTORY.rst').read(),
+    author='Kenneth Reitz',
+    author_email='kenneth@heroku.com',
+    url='https://github.com/heroku/heroku.py',
+    packages=['heroku'],
+    package_data={'': ['LICENSE',]},
+    include_package_data=True,
+    install_requires=required,
+    license='MIT',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+    ),
+)
