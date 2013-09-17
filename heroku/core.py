cd6ca0a398c878bae6b000b37f9e84860448b8cf
Martyzz1 <martyzz1@gmail.com>
2013-09-17 13:18:12 +0100

    pep8 it

--- heroku/core.py
+++ heroku/core.py
@@ -9,6 +9,7 @@ This module provides the base entrypoint for heroku.py.
 
 from .api import Heroku
 
+
 def from_key(api_key, **kwargs):
     """Returns an authenticated Heroku instance, via API Key."""
 
@@ -19,11 +20,13 @@ def from_key(api_key, **kwargs):
 
     return h
 
+
 def from_pass(username, password):
     """Returns an authenticated Heroku instance, via password."""
 
     key = get_key(username, password)
     return from_key(key)
+
 
 def get_key(username, password):
     """Returns an API Key, fetched via password."""
